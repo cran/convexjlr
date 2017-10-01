5cb39c1be72199c4c5e93b2e8e8f6a06b3a0efa4
Changcheng Li <cxl508@psu.edu>
2017-10-01 20:27:41 +0000

    version 0.6.1

--- R/first.R
+++ R/first.R
@@ -2,6 +2,7 @@
 .convex$vars <- 0
 .convex$ps <- 0
 .convex$exprs <- 0
+.convex$objs <- 0
 .convex$status <- FALSE
 
 .check <- function(pkgname){
@@ -25,24 +26,56 @@
 
 .check_installs <- Vectorize(.check_install)
 
-.start <- function(){
-    ## stopifnot(XRJulia::findJulia(test = TRUE))
-    if (!.convex$status) {
-        message("Doing initialization. It may take some time. Please wait.")
-        if (XRJulia::findJulia(test = TRUE)) {
-            .convex$ev <- XRJulia::RJulia()
-            if (all(.check_installs(c("Convex", "SCS")))) {
-                .convex$ev$Command("using Convex")
-                .convex$ev$Command("using SCS")
-                # passing in verbose=0 to hide output from SCS
-                .convex$ev$Command("solver = SCSSolver(verbose=0)")
-                .convex$ev$Command("set_default_solver(solver)")
-                .convex$status <- .convex$ev$Eval("true")
-            }
-            else {message("Packages' installation is not successful.")}
-        }
-        else {message("Julia installation is not found.")}
+.start <- function(backend = c("XRJulia", "JuliaCall")){
+    backend <- match.arg(backend, c("XRJulia", "JuliaCall"))
+    .convex$backend <- backend
+    message("Doing initialization. It may take some time. Please wait.")
+    ## evaluator initialization
+    if (backend == "XRJulia") {
+        stopifnot(XRJulia::findJulia(test = TRUE))
+        .convex$ev <- XRJulia::RJulia()
     }
+    if (backend == "JuliaCall") {
+        ## check is JuliaCall is installed
+        if (!requireNamespace("JuliaCall", quietly = TRUE)) {
+            stop("Package JuliaCall needed for using this backend. Please install it.")
+        }
+        .convex$ev <- JuliaCall::julia_setup()
+        .convex$ev$Command <- .convex$ev$command
+        .convex$ev$Eval <-
+            function(cmd, .get = FALSE) {
+                .convex$ev$eval_string(cmd)
+            }
+        ## if using earlier version of JuliaCall, we need to define the assign function
+        ## to use later.
+        if (is.null(.convex$ev$assign)) {
+            .convex$ev$Command("function assign(name, x) eval(Main, Expr(:(=), Symbol(name), x)) end")
+            .convex$ev$assign <-
+                function(x, value) .convex$ev$call("assign", x, value, need_return = FALSE)
+        }
+    }
+
+    ## Packages
+    if (all(.check_installs(c("Convex", "SCS")))) {
+        .convex$ev$Command("using Convex")
+        .convex$ev$Command("using SCS")
+        # passing in verbose=0 to hide output from SCS
+        .convex$ev$Command("solver = SCSSolver(verbose=0);")
+        .convex$ev$Command("set_default_solver(solver);")
+        .convex$status <- .convex$ev$Eval("true")
+    }
+    else {message("Packages' installation is not successful.")}
+    .convex$status
+}
+
+#' Doing the setup for the package convexjlr (deprecated)
+#'
+#' The function is deprecated, should use convex_setup instead.
+#'
+#' @export
+setup <- function(){
+    .Deprecated("convex_setup")
+    try(.start(), silent = FALSE)
     .convex$status
 }
 
@@ -50,308 +83,19 @@
 #'
 #' This function does the setup for the package convexjlr.
 #' Firstly it will try to establish the connect to Julia via the XRJulia interface,
+#' or try to embed julia in R through JuliaCall.
 #' Secondly it will check for the Julia packages Convex and SCS,
 #' if the packages are not found, it tries to install them into Julia.
-#' Finally, it will try to load the Julia packages and do the neccessary initial setup.
+#' Finally, it will try to load the Julia packages and do the necessary initial setup.
+#'
+#' @param backend whether to use XRJulia or JuliaCall as backend
 #'
 #' @examples
-#' setup()
-#'
+#' \dontrun{
+#' convex_setup()
+#' }
 #' @export
-setup <- function(){
-    try(.start(), silent = TRUE)
+convex_setup <- function(backend = c("XRJulia", "JuliaCall")){
+    try(.start(backend = backend), silent = FALSE)
     .convex$status
 }
-
-join <- function(ls, sep = ", "){
-    do.call(paste, append(ls, list(sep = sep)))
-}
-
-tuple_text <- function(x){
-    paste0("(", join(x), ")")
-}
-
-#' Make a variable to be of Julia's awareness
-#'
-#' Make a variable to be of Julia's awareness, so it can be further used in
-#' the definition of optimization problem.
-#'
-#' @param x the R object sent to Julia
-#' @examples
-#' if (setup()) {
-#'     b <- J(c(1:2))
-#' }
-#' @export
-J <- function(x){
-    r <- .convex$ev$Send(x)
-    structure(x, Jname = r@.Data, proxy = r, class = c(class(x), "shared"))
-}
-
-variable_creator <- function(vtype){
-    force(vtype)
-    function(size = 1, sign = c("None", "Positive", "Negative")){
-        .convex$vars <- .convex$vars + 1
-        sign <- match.arg(sign)
-        sign_text <- switch(sign,
-                            Positive = ", Positive()",
-                            Negative = ", Negative()",
-                            None = "")
-        Jname <- paste0("X_", .convex$vars)
-        command <- paste0(Jname, " = ", vtype, "(", tuple_text(size), sign_text, ")")
-        ## print(command)
-        .convex$ev$Command(command)
-        structure(Jname, size = size,
-                  Jname = Jname,
-                  proxy = .convex$ev$Eval(Jname),
-                  class = "variable")
-    }
-}
-
-#' Create variable for optimization problem
-#'
-#' Create variable (vector, matrix, semidefinite matrix and etc.)
-#' for optimization problem.
-#'
-#' @param size variable size.
-#' @param sign whether variable is element-wise positive, element-wise negative
-#' or neither.
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(4)
-#'     X <- Variable(c(4, 4), sign = "Positive")
-#'     S <- Semidefinite(4)
-#' }
-#' @name variable_creating
-NULL
-
-#' @rdname variable_creating
-#' @export
-Variable <- variable_creator("Variable")
-#' @rdname variable_creating
-#' @export
-Semidefinite <- variable_creator("Semidefinite")
-
-expr <- function(x, env){
-    if (length(x) == 1) {
-        if (x == quote(pmax)) {return(quote(max))}
-        if (x == quote(pmin)) {return(quote(min))}
-        if (x == quote(kronecker)) {return(quote(kron))}
-        if (x == quote(t)) {return(quote(transpose))}
-        r <- try(eval(x, envir = env), silent = TRUE)
-        if (length(attr(r, "Jname")) == 1) {
-            return(eval(parse(text = paste0("quote(", attr(r, "Jname"), ")"))))
-        }
-        if (is.numeric(r) & length(r) == 1) {
-            return(r + 0)
-        }
-        return(x)
-    }
-    for (i in 1:length(x)) {
-        x[[i]] <- expr(x[[i]],env)
-    }
-    x
-}
-
-#' @import magrittr
-
-expr_text <- function(x, env){
-    deparse(expr(x, env)) %>%
-        join(sep = "") %>%
-        gsub(pattern = "\\*", replacement = ".*") %>%
-        gsub(pattern = "/", replacement = "./") %>%
-        gsub(pattern = "\\^", replacement = ".^") %>%
-        gsub(pattern = "%.\\*%", replacement = "*")
-}
-
-#' Create expressions to be used for optimization problem creation
-#'
-#' \code{Expr} create expressions, which can be used later for problem creation.
-#'
-#' @param x expression to be created.
-#'
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(2)
-#'     x1 <- Expr(x + 1)
-#' }
-#' @export
-Expr <- function(x){
-    .convex$exprs <- .convex$exprs + 1
-    expression_text <- expr_text(sys.call()[[2]], env = parent.frame())
-    ## print(ptext)
-    Jname <- paste0("EX_", .convex$exprs)
-    command <- paste0(Jname, " = ", expression_text)
-    ## print(command)
-    .convex$ev$Command(command)
-    structure(Jname, expr = expression_text,
-              command = command,
-              Jname = Jname,
-              proxy = .convex$ev$Eval(Jname),
-              class = "expr")
-}
-
-problem_creator <- function(ptype) {
-    force(ptype)
-    function(...) {
-        .convex$ps <- .convex$ps + 1
-        problem <- lapply(sys.call()[-1], expr_text, env = parent.frame())
-        target <- problem[[1]]
-        constraints <- problem[-1]
-        ptext <- join(problem)
-        ## print(ptext)
-        Jname <- paste0("P_", .convex$ps)
-        command <- paste0(Jname, " = ", ptype, "(", ptext, ")")
-        ## print(command)
-        .convex$ev$Command(command)
-        structure(Jname, problem_type = ptype,
-                  target = target,
-                  constraints = constraints,
-                  command = command,
-                  Jname = Jname,
-                  proxy = .convex$ev$Eval(Jname),
-                  class = "problem")
-    }
-}
-
-#' Create optimization problem
-#'
-#' Create different kinds of optimization problems with
-#' targets and constraints.
-#'
-#' @param ... optimization targets and constraints.
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(4)
-#'     b <- J(c(1:4))
-#'     p <- minimize(sum((x - b) ^ 2), x >= 0, x <= 3)
-#'     p <- maximize(-sum((x - b) ^ 2), x >= 0, x <= 3)
-#'     p <- satisfy(sum((x - b) ^ 2) <= 1, x >= 0, x <= 3)
-#' }
-#' @name problem_creating
-NULL
-
-#' @rdname problem_creating
-#' @export
-minimize <- problem_creator("minimize")
-#' @rdname problem_creating
-#' @export
-maximize <- problem_creator("maximize")
-#' @rdname problem_creating
-#' @export
-satisfy <- problem_creator("satisfy")
-
-#' Solve optimization problem
-#'
-#' \code{cvx_optim} solves optimization problem using Convex.jl.
-#'
-#' @param p optimization problem to be solved.
-#' @return status of optimized problem.
-#'
-#' @examples
-#' if (setup()) {
-#'     x <- Variable()
-#'     b <- 1
-#'     p <- minimize(sum((x - b) ^ 2))
-#'     cvx_optim(p)
-#' }
-#' @export
-cvx_optim <- function(p){
-    .convex$ev$Call("solve!", attr(p, "proxy"))
-    status(p)
-}
-
-#' Add constraints to optimization problem
-#'
-#' \code{addConstraint} add additional constraints to optimization problem.
-#'
-#' @param p optimization problem to add constraints.
-#' @param ... additional constraints.
-#' @return the optimization problem with the additional constraints.
-#'
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(4)
-#'     b <- J(c(1:4))
-#'     p <- minimize(sum((x - b) ^ 2))
-#'     p <- addConstraint(p, x >= 0, x <= 3)
-#' }
-#' @export
-addConstraint <- function(p, ...){
-    stopifnot(attr(p, "class") == "problem")
-    constraints <- lapply(sys.call()[-c(1:2)], expr_text, env = parent.frame())
-    ## p.constraints += [x >= 1; x <= 10; x[2] <= 5; x[1] + x[4] - x[2] <= 10]
-    command <- paste0(attr(p, "Jname"), ".constraints += [", join(constraints, sep = "; "), "]")
-    .convex$ev$Command(command)
-    attr(p, "command") <- append(attr(p, "command"), command)
-    attr(p, "constraints") <- append(attr(p, "constraints"), constraints)
-    p
-}
-
-Jproperty <- function(property){
-    force(property)
-    function(p){
-        stopifnot(length(attr(p, "Jname")) == 1)
-        .convex$ev$Eval(paste0(attr(p, "Jname"), ".", property), .get = TRUE)
-    }
-}
-
-#' Get properties of optimization problem
-#'
-#' Get properties of solved optimization problem,
-#' like the status of problem (optimal, infeasible and etc.),
-#' or the optimal value of the solved optimization problem.
-#'
-#' @param p optimization problem.
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(2)
-#'     b <- J(c(1:2))
-#'     p <- minimize(sum((x - b) ^ 2))
-#'     cvx_optim(p)
-#'     status(p)
-#'     optval(p)
-#' }
-#' @name property
-NULL
-
-#' @rdname property
-#' @export
-status <- Jproperty("status")
-#' @rdname property
-#' @export
-optval <- Jproperty("optval")
-
-#' Get values of expressions at optimizer
-#'
-#' \code{Value} returns the values of expressions at optimizer
-#' (minimizer, maximizer and etc.).
-#'
-#' @aliases evaluate
-#' @param ... expressions needed to evaluate.
-#'
-#' @examples
-#' if (setup()) {
-#'     x <- Variable(4)
-#'     b <- J(c(1:4))
-#'     p <- minimize(sum((x - b) ^ 2))
-#'     cvx_optim(p)
-#'     value(x[1] + x[2], x[3] + x[4])
-#' }
-#' @export
-value <- function(...){
-    original_exprs <- sys.call()[-1]
-    if (length(original_exprs) > 1) {
-        exprs <- lapply(original_exprs, expr_text, env = parent.frame())
-        commands <- paste0("evaluate(", exprs, ")")
-        names(commands) <- lapply(original_exprs, deparse)
-        lapply(commands, .convex$ev$Eval, .get = TRUE)
-    }
-    else {
-        expr <- expr_text(original_exprs[[1]], env = parent.frame())
-        command <- paste0("evaluate(", expr, ")")
-        .convex$ev$Eval(command, .get = TRUE)
-    }
-}
-
-#' @export
-evaluate <- value
--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-Convex Optimization in R by convexjlr
-================
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
+Convex Optimization in R by convexjlr
+=====================================
+
+[![Travis-CI Build Status](https://travis-ci.org/Non-Contradiction/convexjlr.svg?branch=master)](https://travis-ci.org/Non-Contradiction/convexjlr) [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/Non-Contradiction/convexjlr?branch=master&svg=true)](https://ci.appveyor.com/project/Non-Contradiction/JuliaCall) [![CRAN\_Status\_Badge](http://www.r-pkg.org/badges/version/convexjlr)](https://cran.r-project.org/package=convexjlr) [![](http://cranlogs.r-pkg.org/badges/convexjlr)](https://cran.r-project.org/package=convexjlr) [![](https://cranlogs.r-pkg.org/badges/grand-total/convexjlr)](https://cran.r-project.org/package=convexjlr)
+
 `convexjlr` is an `R` package for [Disciplined Convex Programming (DCP)](http://dcp.stanford.edu/) by providing a high level wrapper for Julia package [Convex.jl](https://github.com/JuliaOpt/Convex.jl). The aim is to provide optimization results rapidly and reliably in `R` once you formulate your problem as a convex problem. `convexjlr` can solve linear programs, second order cone programs, semidefinite programs, exponential cone programs, mixed-integer linear programs, and some other DCP-compliant convex programs through `Convex.jl`.
 
-**Installation**:
+Installation
+------------
 
 `convexjlr` is on CRAN now! To use package `convexjlr`, you first have to install Julia <https://julialang.org/> on your computer, and then you can install `convexjlr` just like any other R packages.
+
+Note: `convexjlr` supports multiple ways to connect to `julia`, one way is through package `XRJulia` and the other way is to use package `JuliaCall`. The differences are as follows:
+
+-   `XRJulia` connects to `julia`, which is the default way for `convexjlr`, the advantage is the simplicity of the installation process, once you have a working R and working `julia`, it should be okay to use `convexjlr` in this way. Note that if you have the latest `julia` version (v0.6.0) installed, then you have to use the latest version of `XRJulia` on github: `devtools::install_github("johnmchambers/XRJulia")`.
+
+-   `JuliaCall` embeds `julia` in R, the advantage is the performance, for example, if your convex problem involves large matrice or long vectors, you may wish to use `JuliaCall` backend for `convexjlr`; the disadvantage is the installation process, since embedding `julia` needs compilations, on some types of machines the installation process may be more complicated than `XRJulia`.
+
+And there are several backward incompatible issues in `julia` v0.6 and `julia` v0.5 and corresponding version of `Convex.jl`, so if you are using `julia` v0.6, please use `convexjlr` with care.
 
 We hope you use `convexjlr` to solve your own problems. If you would like to share your experience on using `convexjlr` or have any questions about `convexjlr`, don't hesitate to contact me: <cxl508@psu.edu>.
 
 Quick Example
 -------------
 
-We will show a short example for `convexjlr` in solving linear regression problem. To use package `convexjlr`, we first need to attach it and do the initial setip:
+We will show a short example for `convexjlr` in solving linear regression problem. To use package `convexjlr`, we first need to attach it and do the initial setup:
 
 ``` r
 library(convexjlr)
@@ -22,8 +34,15 @@ library(convexjlr)
 #> The following object is masked from 'package:base':
 #> 
 #>     norm
-setup()
+## If you wish to use JuliaCall backend for performance
+convex_setup(backend = "JuliaCall")
 #> Doing initialization. It may take some time. Please wait.
+#> Julia at location /Applications/Julia-0.6.app/Contents/Resources/julia/bin will be used.
+#> Julia version 0.6.0 found.
+#> Julia initiation...
+#> Finish Julia initiation.
+#> Loading setup script for JuliaCall...
+#> Finish loading setup script for JuliaCall.
 #> [1] TRUE
 ```
 
@@ -41,7 +60,7 @@ linear_regression <- function(x, y){
     beta <- Variable(p)
     b <- Variable()
     ## MSE is mean square error.
-    MSE <- Expr(sum((y - x %*% beta - b) ^ 2) / n)
+    MSE <- Expr(sumsquares(y - x %*% beta - b) / n)
     ## In linear regression, we want to minimize MSE.
     p1 <- minimize(MSE)
     cvx_optim(p1)
@@ -64,12 +83,12 @@ beta0 <- c(5, 1, 0, 2, 0)
 y <- x %*% beta0 + 0.2 * rnorm(n)
 
 linear_regression(x, y)$coef
-#>             [,1]
-#> [1,] 4.998312910
-#> [2,] 1.002886430
-#> [3,] 0.003071815
-#> [4,] 1.989776899
-#> [5,] 0.019197631
+#>              [,1]
+#> [1,]  5.003240727
+#> [2,]  0.991592939
+#> [3,] -0.013119040
+#> [4,]  2.008251896
+#> [5,]  0.004306522
 ```
 
 More Examples
